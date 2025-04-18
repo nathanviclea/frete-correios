2d96ac3f73bc872afac000fa0d6200899900408e
nathanviclea <nathanviclea@gmail.com>
2025-04-17 22:28:32 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,13 +5,16 @@ const { parseStringPromise } = require("xml2js");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Define o CEP fixo de origem (da loja)
+const origem = "08674090"; // CEP da Surfinn
+
 app.get("/", async (req, res) => {
   const cep = req.query.cep;
+
   if (!cep) {
-    return res.status(400).json({ error: "CEP não informado" });
+    return res.status(400).json({ error: "CEP de destino não informado" });
   }
 
-  const origem = "08674090"; // CEP da loja
   const url = `https://ws.correios.com.br/calculador/CalcPrecoPrazo.aspx?nCdEmpresa=&sDsSenha=&nCdServico=04510&sCepOrigem=${origem}&sCepDestino=${cep}&nVlPeso=1&nCdFormato=1&nVlComprimento=20&nVlAltura=5&nVlLargura=15&nVlDiametro=0&sCdMaoPropria=n&nVlValorDeclarado=0&sCdAvisoRecebimento=n&StrRetorno=xml`;
 
   try {
